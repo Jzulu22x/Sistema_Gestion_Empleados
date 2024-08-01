c71e8cd65a854cbd83d59e8e487659b9ffa76d48
Jzulu22x <juanzr1015@gmail.com>
2024-08-01 07:53:16 -0500

    feat: creacion de la clase cliente fix: arreglo de la clase persona

---
+++ Models/Cliente.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace W5_Ejercicios.Models;
+public class Cliente : Persona
+{
+    public string Email { get; set; }
+    public string Telefono { get; set; }
+
+    public Cliente(string nombre, string apellido, string numeroDeIdentificacion, DateOnly fechanacimiento, string email, string telefono) : base (nombre, apellido, numeroDeIdentificacion, fechanacimiento)
+    {
+        Email = email;
+        Telefono = telefono;
+    }
+
+    public override void MostrarInfomacion()
+    {
+        Console.WriteLine($"Información del Cliente:");
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Apellido: {Apellido}");
+        Console.WriteLine($"Número de Identificación: {NumeroDeIdentificacion}");
+        Console.WriteLine($"Fecha de Nacimiento: {FechaNacimiento.ToShortDateString()}");
+        Console.WriteLine($"Edad: {CalcularEdad()} años");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"Teléfono: {Telefono}");
+        Console.WriteLine("----------------------------------------");
+    }
+}
